46c70c6a3b893c164349e31f45f4a2e202b5866e
dliang <1373635050@qq.com>
2020-11-19 16:09:34 +0800

    集合element-ui基本版本

---
+++ src/api/auth.js
@@ -0,0 +1,22 @@
+const TokenKey = 'cpms-Token'
+
+export function getToken() {
+	return sessionStorage.getItem(TokenKey)
+}
+
+export function setToken(token) {
+	return sessionStorage.setItem(TokenKey, token)
+}
+
+export function removeToken() {
+	return sessionStorage.removeItem(TokenKey);
+}
+
+export function removeAllSessionItem() {
+	Object.keys(sessionStorage).forEach(key => {
+		if(key.indexOf('cpms') > -1){
+			sessionStorage.removeItem(key)
+		}
+	} )
+	return true
+}
